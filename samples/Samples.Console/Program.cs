2a8af578cc6b6cae9b0dcf373d4c08960e107395
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-06-16 14:20:55 +0300

    feat: add gemini embedder sample

---
+++ samples/Samples.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using RagLib.Embedders;
+
+namespace Samples.Console;
+
+public class Program
+{
+    static async Task Main(string[] args)
+    {
+        await GeminiEmbedderSamples();
+    }
+
+    private static async Task GeminiEmbedderSamples()
+    {
+        string apiKey = "Add your API key";
+        var embedder = new GeminiEmbedder(apiKey);
+
+        var embedding = await embedder.EmbedAsync("What is RAG?");
+        System.Console.WriteLine(string.Join(", ", embedding));
+    }
+}
