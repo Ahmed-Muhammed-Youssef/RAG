e5f2c4187ba47a60494fa6a1f0a4bc5f8779adaa
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-06-17 13:18:14 +0300

    feat: add support for Microsoft default DI continer

---
+++ src/RagLib/Core/DI/ServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RagLib.Core.Builders;
+using RagLib.Core.Interfaces;
+
+namespace RagLib.Core.DI;
+
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Registers a configured <see cref="IRagEngine"/> instance in the service container using the provided builder configuration.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to register services into.</param>
+    /// <param name="builderConfig">
+    /// A delegate that configures the <see cref="RagEngineBuilder"/> used to construct the <see cref="IRagEngine"/>.
+    /// </param>
+    /// <returns>The same <see cref="IServiceCollection"/> instance so that additional calls can be chained.</returns>
+    public static IServiceCollection AddRagEngine(this IServiceCollection services, Action<RagEngineBuilder> builderConfig)
+    {
+        RagEngineBuilder ragBuilder = RagEngineBuilder.Create();
+
+        builderConfig(ragBuilder);
+
+        var engine = ragBuilder.Build();
+
+        services.AddSingleton<IRagEngine>(engine);
+
+        return services;
+    }
+}
